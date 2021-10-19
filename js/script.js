f24bdc218076528d24830547375dc8800986e415
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-10-19 16:01:04 +0500

    add delete btn

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const elList = document.querySelector('.users__list');
 
 let token  = JSON.parse(window.localStorage.getItem('login'))
 
+// log out from index.html
 if(!token?.token){
     window.location.replace('login.html')
 }
@@ -15,24 +16,57 @@ elLogOutBtn.addEventListener('click', ()=>{
     location.reload()
 })
 
+// localStorage data
+let localData = JSON.parse(window.localStorage.getItem('data'))
+
+
+// async function
 async function renderUsers(){
     const respons = await fetch('https://reqres.in/api/users?page=2')
     const data = await respons.json()
     let resultData = data.data
+    window.localStorage.setItem('data', JSON.stringify(resultData))
+    // let resultData = data.data
+    function renderUser(array, element){
+        element.innerHTML = null;
 
-    resultData.forEach(obj =>{
-        let cloneTemplate = elTemplate.cloneNode(true)
-
-        cloneTemplate.querySelector('.users__img').src = obj.avatar;
-        cloneTemplate.querySelector('#users__name').textContent = obj.first_name;
-        cloneTemplate.querySelector('#users__surmane').textContent = obj.last_name;
-        let userEmail = cloneTemplate.querySelector('.users__email')
-        userEmail.textContent = obj.email
-        console.log(userEmail.setAttribute('href', `mailto: ${obj.email}`))
-
-        elList.appendChild(cloneTemplate)
-        // console.log(obj)
+        array.forEach(obj =>{
+            let cloneTemplate = elTemplate.cloneNode(true)
+            
+            cloneTemplate.querySelector('.users__img').src = obj.avatar;
+            cloneTemplate.querySelector('#users__name').textContent = obj.first_name;
+            cloneTemplate.querySelector('#users__surmane').textContent = obj.last_name;
+            let userEmail = cloneTemplate.querySelector('.users__email')
+            userEmail.textContent = obj.email
+            userEmail.setAttribute('href', `mailto: ${obj.email}`)
+            let deleteBtn = cloneTemplate.querySelector('#user__id')
+            deleteBtn.dataset.id = obj.id
+            
+            deleteBtn.addEventListener('click', (e)=>{
+                async function deleteUser(){
+                    const resDetele = await fetch('https://reqres.in/api/users/'  + e.target.dataset.id, {
+                    method: 'DELETE',
+                })
+                console.log(resDetele.status)
+                if(resDetele.status === 204){
+                    let userId = e.target.dataset.id
+                    
+                    let findUser = localData.findIndex(user => user.id == userId)
+                    
+                    resultData.splice(findUser, 1)
+                    window.localStorage.setItem('data', JSON.stringify(resultData))
+                    localData = JSON.parse(window.localStorage.getItem('data'))
+                    renderUser(localData, elList)
+                    // location.reload()
+                }
+            }
+            deleteUser()
+        })
+        
+        element.appendChild(cloneTemplate)
     })
+}
+renderUser(localData, elList)
 
 }
 renderUsers()
